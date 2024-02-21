efb8e6ca20c51d695d04ff0cdb16035027b06321
RodrigoFlBraz <rodrigoflbraz@gmail.com>
2024-02-20 21:01:34 -0300

    Exercícios.5

--- fundamentos/exerciciosPrimeiroModulo.js
+++ fundamentos/exerciciosPrimeiroModulo.js
@@ -98,3 +98,21 @@ const divisaoEResto = function(dividendo, divisor){
 divisaoEResto(23,3)
 
 
+/*
+05) Lidar com números em JavaScript pode dar muita dor de cabeça. Você já viu o que acontece quando faz o
+seguinte comando no console: console.log(0.1 + 0.2); O resultado será: 0.30000000000000004. Outra coisa
+importante de observar, é o fato que o ponto é utilizado no lugar da vírgula e vice versa. Com isso, vamos fazer
+um exercício simples para mostrar dinheiro sempre da forma correta. Desenvolva uma função JavaScript para
+que ela receba um valor como 0.30000000000000004 e retorne R$0,30 (observe a vírgula e o ponto).
+*/
+
+const formataDinheiro = function(valor){
+    let valorInteiro = Math.floor(valor)
+    let valorDecimal = valor - valorInteiro
+
+    let valor_formatado = 'R$ ' + valorInteiro.toString() + ',' + valorDecimal.toString().slice(2,4)
+    
+    console.log(valor_formatado)
+}
+
+formataDinheiro(9677667676766.30000000000000004)
