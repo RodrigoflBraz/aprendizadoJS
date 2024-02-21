18efe6828e20fb3bb88f08563d9cb0187c67ebf7
RodrigoFlBraz <rodrigoflbraz@gmail.com>
2024-02-20 22:00:02 -0300

    Exercício.8

--- fundamentos/exerciciosPrimeiroModulo.js
+++ fundamentos/exerciciosPrimeiroModulo.js
@@ -181,3 +181,54 @@ const calcularBhaskara = function(ax, bx, c){
 }
 
 console.log(calcularBhaskara(3, 55, 2))
+
+
+
+/*
+08) Pedro joga N jogos de basquete por temporada. Para saber como está ele está progredindo, ele mantém
+registro de todos os as pontuações feitas por jogo. Após cada jogo ele anota no novo valor e confere se o
+mesmo é maior ou menor que seu melhor e pior desempenho. Dada uma lista string = “pontuação1 pontuação2
+pontuação3 etc..”, escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
+vetor com o número de vezes que ele bateu seu recorde de maior número de pontos e quando fez seu pior
+jogo. (Número do pior jogo).
+Obs.: O primeiro jogo não conta como novo recorde do melhor.
+Exemplo:
+String: “10 20 20 8 25 3 0 30 1”
+Retorno: [3, 7] (Significa que ele bateu três vezes seu recorde de melhor pontuação e a pior pontuação
+aconteceu no sétimo jogo.)
+*/
+
+const calculaRecords = function(stringRecordes){
+
+    let vetorDePontos = stringRecordes.split(" ").map(Number)
+
+    let piorPontuacao = vetorDePontos[0];
+    let indicePiorPontuacao = 0;
+    let maiorPontuacao = vetorDePontos[0]
+    let quantidadeRecordsBatidos = 0;
+
+    let vetorRetorno = []
+
+    for(let a = 0; a < vetorDePontos.length; a++){
+        
+        if(vetorDePontos[a] > maiorPontuacao){
+            quantidadeRecordsBatidos ++
+            maiorPontuacao = vetorDePontos[a]
+            console.log("Cheguei aqui a: ", a, "vetor: ", vetorDePontos[a], 'Maior: ', maiorPontuacao)
+        }
+
+        else if(vetorDePontos[a] < piorPontuacao){
+            piorPontuacao = vetorDePontos[a]
+            indicePiorPontuacao = a
+
+        }
+        
+    }
+
+    vetorRetorno = [quantidadeRecordsBatidos, indicePiorPontuacao + 1]
+    return vetorRetorno
+}
+
+console.log(calculaRecords("10 20 20 8 25 3 0 30 1"))
+
+
