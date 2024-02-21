bc2c32e56ccb85d66d9813257a1312a6629fdd8c
RodrigoFlBraz <rodrigoflbraz@gmail.com>
2024-02-20 22:19:42 -0300

    Exercício.9

--- fundamentos/exerciciosPrimeiroModulo.js
+++ fundamentos/exerciciosPrimeiroModulo.js
@@ -16,7 +16,7 @@ const operations = function(a, b){
 }
 
 
-//operations(5, 0)
+operations(5, 5)
 
 
 
@@ -219,7 +219,6 @@ const calculaRecords = function(stringRecordes){
         else if(vetorDePontos[a] < piorPontuacao){
             piorPontuacao = vetorDePontos[a]
             indicePiorPontuacao = a
-
         }
         
     }
@@ -232,6 +231,32 @@ console.log(calculaRecords("10 20 20 8 25 3 0 30 1"))
 
 /*
 
+09) Construa uma função para um sistema de notas de uma instituição que possui a seguinte política de
+classificação: Todo aluno recebe uma nota de 0 a 100. Alunos com nota abaixo de 40 são reprovados. As notas
+possuem a seguinte regra de arredondamento: Se a diferença entre a nota e o próximo múltiplo de 5 for menor
+que 3, arredondar a nota para esse próximo múltiplo de 5. Se a nota for abaixo de 38, não é feito nenhum
+arredondamento pois esta nota resulta na reprovação do aluno. Por exemplo, a nota 84 será arredondada para
+85, mas a nota 29 não será arredondada por ser abaixo de 40 e não ser possível arredondamento eficiente, ou
+seja, que evite a reprovação do aluno. No caso de a nota ser 38, o arredondamento é possível pois atingirá 40
+e o aluno será aprovado.
 
 
 */
+
+
+const arredondaNotas = function(nota){
+    if(nota < 38){
+        return nota
+    }
+
+    const diferencaParaProximoMultiplo = nota % 5
+
+    if (diferencaParaProximoMultiplo >= 3) {
+        return nota + 5 - diferencaParaProximoMultiplo
+    }
+    else{
+        return nota
+    }
+}
+
+console.log(arredondaNotas(83))
